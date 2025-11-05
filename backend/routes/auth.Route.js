8cd7f64d4aa178f8ad5b5ea1b6cd82f29fb0b654
Edward_Bacara <tutorship57@gmail.com>
2025-11-05 14:35:52 +0700

    feat:create Docker File

--- backend/routes/auth.Route.js
+++ backend/routes/auth.Route.js
@@ -8,14 +8,5 @@ router.post('/register',register)
 router.post('/login',login)
 router.post('/logout',logout)
 router.post('/refreshToken',tokenRecover)
-router.get("/getTokenTest", (req, res) => {
-        const {user_id}= req.body
-        try {
-          const accessToken = jwt.sign({ id: user_id }, process.env.JWT_ACCESS_SECRET, { expiresIn: "15m" });
-          res.status(200).json({ accessToken });
-        } catch (error) {
-          res.status(500).json({ message: "Internal server error" });
-        }
-});
 
 module.exports = router;
